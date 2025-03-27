18b4cd4cb668c53445ea1d5a093707c60aeadf98
srisabarikkumar@gmail.com <srisabarikkumar@gmail.com>
2025-03-27 21:29:05 +0530

    new change

--- frontend/src/components/SignInOAuthButtons.tsx
+++ frontend/src/components/SignInOAuthButtons.tsx
@@ -16,7 +16,7 @@ const SignInOAuthButtons = () => {
 
   return <Button onClick={signInWithGoogle} variant={"secondary"} className="w-full 
   text-white border-zinc-200 h-11">
-    <img src="/google.png" alt="google" className="size-5" />
+    <img src="/google.png" alt="google" className="size-5" />{" "}
     Continue with Google
   </Button>;
 }
